d9f0085bc075a882e861ec147d598a8dac9e7ad0
judhisahoo <judhisahoo@gmail.com>
2025-11-25 00:02:08 +0530

    fixed view port warning, pergue warning for tailwaind css and remove old axios file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from './context/AuthContext';
@@ -9,9 +9,16 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Voter Data Management System',
-  description: 'Secure voter data management and search system',
-  viewport: 'width=device-width, initial-scale=1',
+  description: 'Secure voter data management and search system'
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false, // optional
+  themeColor: '#3b82f6', // optional
+}
 
 export default function RootLayout({
   children,
--- app/voter-data-manage-login/page.tsx
+++ app/voter-data-manage-login/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import axios from '@/app/lib/axios'; //'@/lib/axios';
 import Cookies from 'js-cookie';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { FaShieldHalved } from 'react-icons/fa6'; // Changed to fa6
